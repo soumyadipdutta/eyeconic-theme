c4e6d3ba21201b8c66eabcc2d5fd9545dd8725cd
soumyadip1174 <soumyadip@capitalnumbers.com>
2019-02-09 11:33:45 +0530

    Uploaded latest files

---
+++ src/js/custom.js
@@ -0,0 +1,25 @@
+/****************/
+/*CUSTOM JS FILE*/
+/****************/
+
+$(document).ready(function(){
+  $('.expand').click(function(){
+    $('.o-app').toggleClass('expand-main');
+  });
+});
+
+
+
+$(window).load(function(){
+   
+});
+
+
+$(window).scroll(function(){
+    
+	
+});
+
+$(window).resize(function() {
+
+});
